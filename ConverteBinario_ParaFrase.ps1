28412e51f80e90c281cb65fc5d1eabe67cbb5d25
Andrei Henrique Possamai <68831958+opossamai@users.noreply.github.com>
2022-09-23 20:51:18 -0300

    Script que converte um código binário em uma frase legível
    Criado em sala de aula, em menos de 15 minutos, para uma competição.

---
+++ ConverteBinario_ParaFrase.ps1
@@ -0,0 +1,25 @@
+﻿$binario = "";
+$BinarioComEspaco = "";
+$i = 0;
+
+[string]$binario.Trim() -split "" | ForEach-Object {
+    
+    if($_){
+        if($i -eq 8){
+            $BinarioComEspaco += " ";
+            $i = 0;
+        }
+
+        $BinarioComEspaco += [string]$_;
+        $i += 1;
+    }
+}
+
+$Frase = ""
+
+$BinarioComEspaco -split " " | ForEach-Object {
+    $Decimal = [convert]::ToInt32($_,2);
+    $Frase += [char]$Decimal
+}
+
+$Frase
